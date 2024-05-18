df45596bc0148d2b837547838ee70c1b170c3d62
TuanQuynh0308 <117746207+TuanQuynh0308@users.noreply.github.com>
2024-05-18 08:55:16 +0700

    v5

--- app/src/main/java/com/example/lab3_ph31990/BTapBuoi5.kt
+++ app/src/main/java/com/example/lab3_ph31990/BTapBuoi5.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -19,11 +20,16 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,30 +42,49 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.material.internal.ViewUtils.RelativePadding
 
 
 class BTapBuoi5 : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GetLayout()
+        setContent {Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color(0xff252121),
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        GetTextTitle("Thanh toán")
+                    }
+                )
+            },
+        ) { innerPadding ->
+            GetLayout(innerPadding)
+        }
+
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GetLayout(){
+fun GetLayout(innerPadding: PaddingValues = PaddingValues()){
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
+            .padding(
+                top = innerPadding.calculateTopPadding()
+            )
             .background(Color(0xFF262121))
-            .padding(10.dp),
+            .padding(5.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        GetTextTitle("Thanh Toán")
+//        GetTextTitle("Thanh Toán")
         Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = "Địa chỉ nhận hàng ",
@@ -102,13 +127,14 @@ fun GetLayout(){
 
         Button(
             onClick = {},
-            colors = ButtonDefaults.buttonColors(Color(0xFFFFA726)),
+            colors = ButtonDefaults.buttonColors(Color(0xFFfe734c)),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
             Text(text = "Tiếp theo", color = Color.White)
         }
+        Spacer(modifier = Modifier.weight(1f))
         BottomNavigationBar()
     }
 }
@@ -124,9 +150,8 @@ fun RowDiaChiNhanHang(){
     ) {
         Image(
             painter = painterResource(id = R.drawable.vitri),
-            contentDescription = "Vị trí ",
+            contentDescription = null,
             modifier = Modifier.size(40.dp)
-
 
         )
         DiaChiNhanHang()
@@ -144,13 +169,14 @@ fun ThanhToan(name: String, logo: Int,bg:Color){
         Image(
             painter = painterResource(id = logo),
             modifier = Modifier
-                .size(55.dp)
+                .size(56.dp)
                 .padding(4.dp),
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
         Spacer(modifier = Modifier.width(16.dp))
         Text(text = name,color = Color.White, modifier = Modifier.weight(1f))
+
         RadioButton(selected = false, onClick = {})
     }
 }
@@ -190,7 +216,7 @@ fun DiaChiNhanHang() {
 @Composable
 fun BottomNavigationBar() {
     NavigationBar(
-        containerColor = Color(0xFF262121),
+        containerColor = Color(0xFF312c2c),
         contentColor = Color.White
     ) {
         NavigationBarItem(
